d0e20f8c4f3aaf648f438b1a2ce78cb7b023ace4
scott <devnull@localhost>
2011-03-08 15:08:06 -0600

    first

---
+++ Summarizer.Presentation/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace Summarizer.Presentation
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
