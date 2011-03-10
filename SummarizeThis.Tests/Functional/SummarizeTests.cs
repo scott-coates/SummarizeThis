487cdc70a97e5fa05b74ca7dc87c3e6fdbbf487f
scott <devnull@localhost>
2011-03-10 16:37:31 -0600

    most frequent

--- SummarizeThis.Tests/Functional/SummarizeTests.cs
+++ SummarizeThis.Tests/Functional/SummarizeTests.cs
@@ -33,5 +33,27 @@ namespace SummarizeThis.Tests.Functional
             string result = _summarizer.Summarize(input, 1).MostFrequentWords.First();
             Assert.AreEqual(expectedResult, result);
         }
+
+        [Test]
+        public void TwoSentenceReturnLongerInput()
+        {
+            const string input = "NClassifier is a dotnet assembly for working with text. NClassifier includes a summarizer. A Summarizer allows the summary of text. A Summarizer is really cool. I don't think there are any other dotnet summarizers.";
+            const string expectedResult = "NClassifier is a dotnet assembly for working with text. NClassifier includes a summarizer.";
+            
+            string result = _summarizer.Summarize(input, 2).SummarizedText;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void OneSentenceReturnLongerInput()
+        {
+            const string input = "NClassifier is a dotnet assembly for working with text. NClassifier includes a summarizer. A Summarizer allows the summary of text. A Summarizer is really cool. I don't think there are any other dotnet summarizers.";
+            const string expectedResult = "NClassifier includes a summarizer.";
+
+            string result = _summarizer.Summarize(input, 1).SummarizedText;
+
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
