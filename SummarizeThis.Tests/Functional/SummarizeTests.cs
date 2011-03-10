52e5ab7fd22fdfec18e0eaf051d1cf66ca8c32d2
scott <devnull@localhost>
2011-03-10 16:32:19 -0600

    updated 1

--- SummarizeThis.Tests/Functional/SummarizeTests.cs
+++ SummarizeThis.Tests/Functional/SummarizeTests.cs
@@ -1,9 +1,27 @@
 ﻿using NUnit.Framework;
+using SummarizeThis.Core.Summarization;
+using SummarizeThis.Core.Summarization.Interfaces;
 
 namespace SummarizeThis.Tests.Functional
 {
     [TestFixture]
     public class SummarizeTests
     {
+        private ISummarizer _summarizer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _summarizer = new Summarizer();
+        }
+
+        [Test]
+        public void OneSentenceReturnShortInput()
+        {
+            const string input = "NClassifier is a dotnet assembly for working with text.  NClassifier includes a summarizer.";
+            const string expectedResult = "NClassifier is a dotnet assembly for working with text.";
+            string result = _summarizer.Summarize(input, 1).SummarizedText;
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
