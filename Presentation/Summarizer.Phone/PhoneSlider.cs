24c22b61b97ee715e0cef15acb083853551cd694
scott <devnull@localhost>
2011-03-14 18:34:06 -0500

    added new slider

---
+++ Presentation/Summarizer.Phone/PhoneSlider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Summarizer.Phone
+{
+    public class PhoneSlider : Slider
+    {
+        public PhoneSlider()
+        {
+            SizeChanged += new SizeChangedEventHandler(PhoneSlider_SizeChanged);
+        }
+
+        void PhoneSlider_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width > 0 && e.NewSize.Height > 0)
+            {
+                Rect clipRect = new Rect(0, 0, e.NewSize.Width, e.NewSize.Height);
+                if (Orientation == Orientation.Horizontal)
+                {
+                    clipRect.X -= 12;
+                    clipRect.Width += 24;
+                    object margin = Resources["PhoneHorizontalMargin"];
+                    if (margin != null)
+                    {
+                        Margin = (Thickness)margin;
+                    }
+                }
+                else
+                {
+                    clipRect.Y -= 12;
+                    clipRect.Height += 24;
+                    object margin = Resources["PhoneVerticalMargin"];
+                    if (margin != null)
+                    {
+                        Margin = (Thickness)margin;
+                    }
+                }
+
+                this.Clip = new RectangleGeometry() { Rect = clipRect };
+            }
+        }
+    }
+}
